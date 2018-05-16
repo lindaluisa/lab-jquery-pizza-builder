3283621f9406a1546f05755a8d7a574be50c4c4f
lindaluisa <lindaluisarosenthal@gmail.com>
2018-05-16 22:54:10 +0200

    updating price list, hiding and showing selected items

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -12,26 +12,32 @@
   var $mushroom = $('.mushroom');
   var $greenPeppers = $('.green-pepper');
 
+  // Price
+  var $pepPrice = $('li:contains("pepperonni")')
 
-  $pepperonni.toggle()
-  $pepBtn.removeClass('active');
+
+  // $pepperonni.toggle()
+  // $pepBtn.removeClass('active');
   $pepBtn.on('click', function(){
     $pepperonni.toggle();
     $pepBtn.toggleClass('active');
+    $('li:contains("pepperonni")').toggle();
   });
 
-  $mushroom.toggle();
-  $mushBtn.removeClass('active');
+  // $mushroom.toggle();
+  // $mushBtn.removeClass('active');
   $mushBtn.on('click', function(){
     $mushroom.toggle();
     $mushBtn.toggleClass('active');
+    $('li:contains("mushrooms")').toggle();
   });
 
-  $greenPeppers.toggle();
-  $greenPepBtn.removeClass('active');
+  // $greenPeppers.toggle();
+  // $greenPepBtn.removeClass('active');
   $greenPepBtn.on('click', function(){
     $greenPeppers.toggle();
     $greenPepBtn.toggleClass('active');
+    $('li:contains("green peppers")').toggle();
   });
 
 // White Sauce & Gluten Free Crust
@@ -42,9 +48,12 @@ var $sauceBtn = $('.btn-sauce');
   $sauce.removeClass('sauce-white');
   $sauceBtn.removeClass('active');
 
+  $('li:contains("white sauce")').hide();
   $sauceBtn.on('click', function() {
   $sauce.toggleClass('sauce-white');
   $sauceBtn.toggleClass('active');
+  $('li:contains("white sauce")').toggle();
+
 })
 
 var $crust = $('.crust');
@@ -53,9 +62,13 @@ var $crustBtn = $('.btn-crust');
   $crust.removeClass('crust-gluten-free');
   $crustBtn.removeClass('active');
 
+
+  $('li:contains("gluten-free crust")').hide();
   $crustBtn.on('click', function(){
     $crust.toggleClass('crust-gluten-free');
     $crustBtn.toggleClass('active');
+    $('li:contains("gluten-free crust")').toggle();
+
   })
 
 
